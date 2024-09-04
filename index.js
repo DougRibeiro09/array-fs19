3ae97bcb8166b16e94fa1b554ee734b837747589
Douglas Eugenio Ribeiro dos Santos <douglas0503@digitalcollegesul.local>
2024-09-04 20:11:55 -0300

    Inicio dos metodos com array

---
+++ index.js
@@ -0,0 +1,24 @@
+// let listaJogos = ['Lost Planet 3', 'Crash 3', 'Ace Combat']
+
+// let listaEstrelas = [{nome: 'Angelo', idade: 12}, 'Betegeuse', 'Sirium','Anilam', 12]
+
+// // 2 - Alterar o ultimo item para maiusculo
+
+// console.log(listaJogos[2].toUpperCase())
+
+// 3 - Alterar todos os elementos para maiusculo
+let pessoas = [
+    'AnGELO lustosa',
+    'Juan bEnJamin',
+    'MAteus ArANHA'
+]
+// 3.1 - Map com arrow function
+// pessoas.map((item) => console.log(item.toUpperCase()))
+
+// 3.2 - forEach sem arrow function
+// pessoas.map(function(item) {
+// return console.log(item.toUpperCase())
+// })
+
+//4 - Adicionar item no array
+
