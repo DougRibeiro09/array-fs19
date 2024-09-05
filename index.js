66b6e645630df7ead601e3a23b341777ddf91bc0
Douglas Eugenio Ribeiro dos Santos <douglas0503@digitalcollegesul.local>
2024-09-04 22:26:06 -0300

    estou adicionando metodos com array

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-// let listaJogos = ['Lost Planet 3', 'Crash 3', 'Ace Combat']
+// 1 - Criar uma lista vazia
+let listaJogos = [890, 'Crash 3', 'Ace Combat']
 
-// let listaEstrelas = [{nome: 'Angelo', idade: 12}, 'Betegeuse', 'Sirium','Anilam', 12]
+let listaEstrelas = [{nome: 'Angelo', idade: 12}, 'Betegeuse', 'Sirius', 'Anilam', 12]
 
-// // 2 - Alterar o ultimo item para maiusculo
+// 2 - Alterar o último item para maiusculo
 
-// console.log(listaJogos[2].toUpperCase())
+//console.log(listaJogos[2].toUpperCase());
 
-// 3 - Alterar todos os elementos para maiusculo
+// 3 - Alterar todos os elentos para maiusculo
 let pessoas = [
     'AnGELO lustosa',
     'Juan bEnJamin',
-    'MAteus ArANHA'
+    'MAteuS ArANHA'
 ]
+
 // 3.1 - Map com arrow function
-// pessoas.map((item) => console.log(item.toUpperCase()))
+//pessoas.map((item) => console.log(item.toUpperCase()))
 
 // 3.2 - forEach sem arrow function
-// pessoas.map(function(item) {
-// return console.log(item.toUpperCase())
-// })
+/* pessoas.forEach(function(item) {
+    return console.log(item.toUpperCase());
+})*/
 
-//4 - Adicionar item no array
-
+// 4 - Adicionar item no array
